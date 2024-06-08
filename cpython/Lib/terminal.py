2a08eae8e8127506089796af4f4d94dfa1f10ea3
Feldwor <BoQsc@users.noreply.github.com>
2024-06-08 08:55:18 +0300

    Update terminal.py

--- cpython/Lib/terminal.py
+++ cpython/Lib/terminal.py
@@ -6,9 +6,20 @@ import msvcrt
 def readkey():
     return msvcrt.getwch()
 
+# First version and concise
+# def readkeyuntil(text, keys):
+#     while list(text) != keys:
+#         keys.append(readkey())
+#         print(keys)
+# 
+
+# Second version and more handling support
 def readkeyuntil(text, keys):
-    while list(text) != keys:
-        keys.append(readkey())
-        print(keys)
+    while True:
+        if list(text) != keys:
+            keys.append(readkey())
+            print(keys)
+        else:  
+            return
 
 readkeyuntil("waffle", [])
