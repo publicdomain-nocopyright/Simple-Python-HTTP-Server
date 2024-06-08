d2369446dd6959b5a20ed54df1ac4faa48760dd3
Feldwor <BoQsc@users.noreply.github.com>
2024-06-08 15:51:47 +0300

    Update terminal.py

--- cpython/Lib/terminal.py
+++ cpython/Lib/terminal.py
@@ -109,7 +109,7 @@ def readkeyuntil(text, keys):
     while True:
         # Read key input and append to keys list
         lastkey = readkey()
-        print(lastkey, end="|")
+        print(lastkey, end="\r")
         keys.append(lastkey)
         #print(arraytostring(keys))
         
